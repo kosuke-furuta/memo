e9d79a9df9881ab3fe80bb9aef363a5c81d203ba
kosuke-furuta <furu.21.22@gmail.com>
2021-02-16 05:37:56 +0900

    heroku push

--- app/models/taskmemo.rb
+++ app/models/taskmemo.rb
@@ -3,7 +3,7 @@ class Taskmemo < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :product_name, presence: true, length: { maximum: 20 }
-  validates :order_name, presence: true, length: length: { maximum: 6 }
+  validates :order_name, presence: true, length: { maximum: 6 }
   validates :delivery_date, presence: true
   validates :quantity, presence: true
   validates :product_name, presence: true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'your-domain' }
+  config.action_mailer.default_url_options = { :host => 'taskmemo-20210216' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.gmail.com',
