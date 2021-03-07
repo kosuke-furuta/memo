0b2708d80053b392d6e709c4e3bcb163e306846a
kosuke-furuta <furu.21.22@gmail.com>
2021-03-08 06:54:13 +0900

    following-taskmemos forth save on the way

--- app/models/taskmemo.rb
+++ app/models/taskmemo.rb
@@ -13,16 +13,16 @@ class Taskmemo < ApplicationRecord
                     size: { less_than: 5.megabytes,
                             message: "5MB未満である必要があります" }
 
-has_many :active_relationships, class_name: "Relationship",
-                                foreign_key: "follower_id",
-                                dependent:   :destroy
-has_many :passive_relationships, class_name: "Relationship",
-                                 foreign_key: "followed_id",
-                                 dependent: :destroy
-has_many :following, through: :active_relationships, source: :followed
-has_many :followers, through: :passive_relationships, source: :follower
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
-# 表示用のリサイズ済み画像を返す
+  # 表示用のリサイズ済み画像を返す
   def display_image
     image.variant(resize_to_limit: [500, 500])
   end
@@ -41,12 +41,12 @@ has_many :followers, through: :passive_relationships, source: :follower
   def following?(other_taskmemo)
     following.include?(other_taskmemo)
   end
-end
 
-# ユーザーのステータスフィードを返す
-def feed
-  following_ids = "SELECT followed_id FROM relationships
-                   WHERE follower_id = :taskmemo_id"
-  Taskmemo.where("taskmemo_id IN (#{following_ids})
-                  OR taskmemo_id = :taskmemo_id", taskmemo_id: id)
+  # タスクメモのステータスフィードを返す
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :taskmemo_id"
+    Taskmemo.where("taskmemo_id IN (#{following_ids})
+                    OR taskmemo_id = :taskmemo_id", taskmemo_id: id)
+  end
 end
